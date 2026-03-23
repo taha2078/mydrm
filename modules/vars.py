3a58ac29f4c80e458eae976f23710321eb946ff7
taha2078 <tahamomin2007@gmail.com>
2026-03-23 12:24:47 +0530

    Refactor environment variable handling in vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,18 +3,18 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "21190777"))
-API_HASH = environ.get("API_HASH", "7308a1cb7ac83b0cc17530de780a6969")
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "7198711003"))
+OWNER = int(environ.get("OWNER", ""))
 CREDIT = environ.get("CREDIT", "TAHA MOMIN")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '7198711003').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '7198711003').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
