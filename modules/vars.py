5ce16d50b504a43d97f70c3f6c0accdfbe190d31
taha2078 <tahamomin2007@gmail.com>
2026-03-24 19:53:54 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -7,14 +7,14 @@ API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", ""))
-CREDIT = environ.get("CREDIT", "TAHA MOMIN")
+OWNER = int(environ.get("OWNER", "7198711003"))
+CREDIT = environ.get("CREDIT", "✨TAHA MOMIN™")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7198711003').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7198711003').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
